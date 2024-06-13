d3888e070dd27e810c06a7f22f270793c3392772
noellopez <noel.lopez@mqaamericas.com>
2024-06-13 15:39:38 -0600

    add: rotate piece

--- tetris.js
+++ tetris.js
@@ -64,6 +64,20 @@ function collide(arena, player) {
     return false;
 }
 
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
+        }
+    }
+
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
+
 function createPiece(type) {
     if (type === 'T') {
         return [
