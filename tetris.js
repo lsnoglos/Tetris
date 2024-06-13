b08278d316de8efd908208f32d31b86738a76673
noellopez <noel.lopez@mqaamericas.com>
2024-06-13 15:52:04 -0600

    add: detected complete line function

--- tetris.js
+++ tetris.js
@@ -32,6 +32,24 @@ function drawMatrix(matrix, offset) {
     });
 }
 
+function arenaSweep() {
+    let rowCount = 1;
+    outer: for (let y = arena.length - 1; y > 0; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0) {
+                continue outer;
+            }
+        }
+
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+
+        player.score += rowCount * 10;
+        rowCount *= 2;
+    }
+}
+
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
