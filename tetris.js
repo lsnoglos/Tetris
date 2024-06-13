442d5028ea913e1376d49762e9e61fe08d9d19ae
noellopez <noel.lopez@mqaamericas.com>
2024-06-13 15:06:19 -0600

    create matrix

--- tetris.js
+++ tetris.js
@@ -4,3 +4,13 @@ const context = canvas.getContext('2d');
 canvas.width = 300;
 canvas.height = 600;
 context.scale(20, 20);
+
+function createMatrix(width, height) {
+    const matrix = [];
+    while (height--) {
+        matrix.push(new Array(width).fill(0));
+    }
+    return matrix;
+}
+
+const arena = createMatrix(12, 20);
