896cefedce8f42e5656fcd69647e64875f88e402
noellopez <noel.lopez@mqaamericas.com>
2024-06-13 15:48:53 -0600

    add: player move piece function

--- tetris.js
+++ tetris.js
@@ -78,6 +78,28 @@ function rotate(matrix, dir) {
     }
 }
 
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while (collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+    }
+}
+
+function playerMove(offset) {
+    player.pos.x += offset;
+    if (collide(arena, player)) {
+        player.pos.x -= offset;
+    }
+}
+
 function createPiece(type) {
     if (type === 'T') {
         return [
