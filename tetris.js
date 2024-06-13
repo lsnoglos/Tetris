f1f472e8f6cad90d6c71ba91dfc80fd5f31d7c2a
noellopez <noel.lopez@mqaamericas.com>
2024-06-13 15:38:49 -0600

    add: validate if collide with arena

--- tetris.js
+++ tetris.js
@@ -50,6 +50,20 @@ function merge(arena, player) {
     });
 }
 
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos];
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 &&
+                (arena[y + o.y] &&
+                    arena[y + o.y][x + o.x]) !== 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 function createPiece(type) {
     if (type === 'T') {
         return [
